6e9d9ccf05dcd08113503f99f085d5f25304ae46
Augusto Domenech <augustodomenech@gmail.com>
2019-10-06 16:48:32 -0300

    Implementado rotina de update com validações

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -20,14 +20,41 @@ class UserController{
     })
   };
 
+  //Metado de alteração de dados do ususários
   async update(req, res) {
       
-    res.json({ id: req.idUser})
+    //Pegamos o email e a senha antiga do corpo da requisição
+    const {email, oldPassword} = req.body;
+    
+    //Procuramos o usuário no banco 
+    const user = await User.findByPk(req.idUser);
 
-  }
+    //Vericamos se o novo email já existe cadastrado no banco
+    if (email !== user.email){
+      const UsertExist = await User.findOne({where: { email }});
+    
+      if (UsertExist) {
+        return res.status(400).json({error: "User already exists."})
+      };  
+    };
 
+    //Verificamos se a senha bate com a senha atual
+    if ( oldPassword && !(await user.checkPassword(oldPassword)) ) {
+      return res.status(401).json({error: 'Password does not match '})
+    };
+
+    //Realizamos o update
+    const {name, password, provider} = await user.update(req.body);
+
+    //Retornamos o usuário
+    return res.json({
+      name, 
+      email, 
+      password,
+      provider
+    })
+  };
 
 
 };
-
 export default new UserController();
