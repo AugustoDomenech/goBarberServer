8c42865749b75c1209d62fe7055bc5e74dacd623
Augusto Domenech <augustodomenech@gmail.com>
2019-10-08 22:14:36 -0300

    Aplicado rotina de upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from "./app/controller/SessionControler";
 import UserController from "./app/controller/UserController";
@@ -6,6 +8,7 @@ import UserController from "./app/controller/UserController";
 import authMiddleware from './app/middleware/auth'
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/session', SessionController.store)
 routes.post('/users', UserController.store)
@@ -14,5 +17,7 @@ routes.use(authMiddleware);
 
 routes.put('/update', UserController.update)
 
-
+routes.put('/files', upload.single('file'), (req, res) => {
+  return res.json({ok: true})
+})
 export default routes;
