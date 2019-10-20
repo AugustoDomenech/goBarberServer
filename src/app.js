871c0a047c1f183fdfe8fe17301f0e01ab30803c
Augusto Domenech <augustodomenech@gmail.com>
2019-10-20 17:51:32 -0300

    Tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
-import routes from './routes';
 import path from 'path';
+import Youch from 'youch'
+import * as Sentry from '@sentry/node';
+
+import 'express-async-errors';
+
+import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import "./database";
 
@@ -9,11 +15,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.execptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());    
     this.server.use(express.json());
     this.server.use( '/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads'))
 
@@ -21,7 +31,16 @@ class App {
   }
 
   routes() {
+    this.server.use(Sentry.Handlers.errorHandler());
     this.server.use(routes);
+  }
+
+  execptionHandler(){
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      
+      return res.status(500).json(errors);
+    })
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://dd39e8330c5a41f7b1e16d655a10326f@sentry.io/1785195',
+}
